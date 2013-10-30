c26791a25d681d38c0a1b840b346ed5bb17cb84d
Bill Putrino <bputrino@gmail.com>
2013-10-30 11:30:15 -0400

    Added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Billy Putrino on 10/30/13.
+//  Copyright (c) 2013 Four8Five Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Billy Putrino on 10/30/13.
+//  Copyright (c) 2013 Four8Five Studios. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
